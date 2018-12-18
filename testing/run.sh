a72c0d345af9a45792c2ca9c99657bb677a2041e
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2018-12-18 17:08:42 -0500

    edit run

--- testing/run.sh
+++ testing/run.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
 
-curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/reads.bam
-curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/reads.bam.bai
-#
-curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.bam
-curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.bam.bai
-#
-curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz
-curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz.tbi
+
+mkdir -p input_data
+cd input_data
+
+if [ ! -s reads.bam.bai ]
+then
+    curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/reads.bam
+    curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/reads.bam.bai
+fi
+
+if [ ! -s test.bam.bai ]
+then
+    curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.bam
+    curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.bam.bai
+fi
+
+if [ ! -s test.vcf.gz.tbi ]
+then
+    curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz
+    curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz.tbi
+fi
+
+cd -
+
