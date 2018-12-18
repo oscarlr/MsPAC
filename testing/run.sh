430bef1737eaac6437d6717365e7720226bec812
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2018-12-18 17:05:47 -0500

    adding run

---
+++ testing/run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/reads.bam
+curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/reads.bam.bai
+#
+curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.bam
+curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.bam.bai
+#
+curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz
+curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz.tbi
