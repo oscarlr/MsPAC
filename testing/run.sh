5a16c28881e634a12fbdef81eab73b685606a20a
Oscar Rodriguez <oscar.rodriguez@icahn.mssm.edu>
2018-12-18 20:45:25 -0500

    sv calling

--- testing/run.sh
+++ testing/run.sh
@@ -22,10 +22,17 @@ then
     curl -O https://rodrio10.u.hpc.mssm.edu/MsPAC/test.vcf.gz.tbi
 fi
 
+if [ ! -s chr22.fa ]
+then
+    curl -O http://hgdownload.cse.ucsc.edu/goldenpath/hg19/chromosomes/chr22.fa.gz
+    gunzip chr22.fa.gz
+    samtools faidx chr22.fa
+fi
 cd -
 
 ls input_data/reads.bam > reads.fofn
 
-MsPAC phase-bam run.cfg
-MsPAC prep-reads run.cfg
-MsPAC assembly run.cfg
+#MsPAC phase-bam run.cfg
+#MsPAC prep-reads run.cfg
+#MsPAC assembly run.cfg
+MsPAC sv-calling run.cfg
